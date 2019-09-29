c6f673e3940e6aedfc894702c8848616b53ff30a
weslleymatheus05 <55143387+weslleymatheus05@users.noreply.github.com>
2019-09-28 23:43:00 -0300

    Create questao 5 da lista 3.c

---
+++ questao 5 da lista 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	char s[20];
+	char a[20];
+	
+	  printf("Digite o texto aqui: ");
+	  gets(s);
+	
+	  printf("Digite o texto aqui: ");
+	  gets(a);
+	
+	  printf("%s %s", s,a);
+	
+}
