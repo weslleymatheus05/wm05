ff1540292b62be95c178ced92a03f9e29b0cfd5f
weslleymatheus05 <55143387+weslleymatheus05@users.noreply.github.com>
2019-09-30 20:47:10 -0300

    Update questao 4 da lista 3.c

--- questao 4 da lista 3.c
+++ questao 4 da lista 3.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+int main(){
+	
+	int i;
+	char txt1[100];
+	char txt2[100];
+	
+		printf("Digite o texto para ser armazenado em outra variavel: \n");
+		gets(txt1);
+	
+	for(i=0;txt1[i] != '\0' && i<sizeof(txt1); i++){
+		txt2[i]=txt1[i];
+	}
+		printf("O texto que foi armazenado em outra variavel e:\n %s",txt2);
+}
+
