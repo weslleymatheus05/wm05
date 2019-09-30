15c4a7cf164fe3a17fde16ca1bb3314db2a6b88c
weslleymatheus05 <55143387+weslleymatheus05@users.noreply.github.com>
2019-09-30 20:50:10 -0300

    Update questao 3 da lista 3.c

--- questao 3 da lista 3.c
+++ questao 3 da lista 3.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
 
+int main(void){
+	
+	int i, contador = 0;
+	char txt1[100];
+	char txt2[100];
+	
+		printf("Digite um texto para ser comparado com o segundo texto: \n");
+		gets(txt1);
+		
+		printf("Digite o segundo texto; \n");
+		gets(txt2);
+		
+	for(i=0; txt1[i] != '\0' && i<sizeof(txt1); i++){
+		if(txt1[i] != txt2[i]){
+			printf("Os textos sao diferentes");
+			contador++;
+			break;
+		}
+	}
+	if (contador=0){
+		printf("Os textos sao iguais");
+	}
+}
+
