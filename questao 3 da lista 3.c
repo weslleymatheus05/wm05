ce783bebb83e7fcf3ea2f7e6f23e60260a34abe1
weslleymatheus05 <55143387+weslleymatheus05@users.noreply.github.com>
2019-10-01 10:24:59 -0300

    Update questao 3 da lista 3.c

--- questao 3 da lista 3.c
+++ questao 3 da lista 3.c
@@ -6,11 +6,11 @@ int main(void){
 	char txt1[100];
 	char txt2[100];
 	
-		printf("Digite um texto para ser comparado com o segundo texto: \n");
-		gets(txt1);
+	printf("Digite um texto para ser comparado com o segundo texto: \n");
+	gets(txt1);
 		
-		printf("Digite o segundo texto; \n");
-		gets(txt2);
+	printf("Digite o segundo texto; \n");
+	gets(txt2);
 		
 	for(i=0; txt1[i] != '\0' && i<sizeof(txt1); i++){
 		if(txt1[i] != txt2[i]){
